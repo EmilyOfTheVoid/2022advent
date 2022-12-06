4734e2205c1f2e72260e68da0c8852b6721e6a4d
Emily <emily@thisgirlsaphony.com>
2022-12-06 14:00:44 -0600

    day6

--- src/day6/prob2.ts
+++ src/day6/prob2.ts
@@ -1,12 +1,19 @@
 import { readInput } from '../utils/puzzle.js';
 
 async function run(): Promise<string | number> {
-    //setup
-    const input = await readInput('day6');
+    const data = await readInput('day6');
+    const markerLength = 14;
+    const marker = new Set();
+    let i = markerLength - 1;
+    while (marker.size < markerLength && i < data.length) {
+        i++
+        marker.clear();
+        const set = data.slice(i-markerLength, i);
+        set.split('').map(a => marker.add(a));
+    }
 
     //work
-    return 'WIP'
+    return i;
 }
-
 
 export default run;
